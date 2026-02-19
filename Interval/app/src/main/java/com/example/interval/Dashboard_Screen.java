065415a5856bff11911ac8d0b01a14515e337585
nobodydebunny <shihansandakelum@gmail.com>
2026-02-19 18:01:42 +0530

    logout warning finished

--- Interval/app/src/main/java/com/example/interval/Dashboard_Screen.java
+++ Interval/app/src/main/java/com/example/interval/Dashboard_Screen.java
@@ -1,12 +1,19 @@
 package com.example.interval;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class Dashboard_Screen extends AppCompatActivity {
 
@@ -15,6 +22,30 @@ public class Dashboard_Screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard_screen);
+
+        ImageButton logoutbtn = findViewById(R.id.logoutBtn);
+
+        logoutbtn.setOnClickListener(v ->{
+            warning_dialog.show(
+                    this,
+                    "Logout",
+                    "Are you sure you want to log out of your session?",
+                    "Logout",
+                    "Cancel",
+                    R.drawable.ic_logout,
+                    () -> {
+                        Intent intent = new Intent(Dashboard_Screen.this, login_screen.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+            );
+        });
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
